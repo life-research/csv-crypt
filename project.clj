d263ba956db0d70e10f17d1b78205231665bdce7
Alexander Kiel <alexanderkiel@gmx.net>
2018-01-23 13:09:00 +0100

    Add Project Description

--- project.clj
+++ project.clj
@@ -1,5 +1,5 @@
 (defproject csv-crypt "0.1-SNAPSHOT"
-  :description "FIXME: write description"
+  :description "Line-by-line CSV Encryption Tool"
   :url "http://example.com/FIXME"
   :license {:name "Eclipse Public License"
             :url "http://www.eclipse.org/legal/epl-v10.html"}
