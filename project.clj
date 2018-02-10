b848025d0bcab2c0ea986006aacd6910abb9ab67
Alexander Kiel <alexanderkiel@gmx.net>
2018-02-10 14:38:43 +0100

    Bump Next Dev Version

--- project.clj
+++ project.clj
@@ -1,4 +1,4 @@
-(defproject csv-crypt "0.2"
+(defproject csv-crypt "0.3-SNAPSHOT"
   :description "Line-by-line CSV Encryption Tool"
   :url "https://github.com/life-research/csv-crypt"
   :license {:name "Eclipse Public License"
